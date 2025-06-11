2928222705a15b274489ada3430317f0ebe900f8
sanikani <sanikani@naver.com>
2025-06-11 17:01:52 +0900

    [feat] : 포인트 엔티티 설계
    - Point Field 수정

--- src/main/java/shop/sajotuna/order/point/domain/Point.java
+++ src/main/java/shop/sajotuna/order/point/domain/Point.java
@@ -1,6 +1,9 @@
 package shop.sajotuna.order.point.domain;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PositiveOrZero;
 import lombok.Getter;
 
 import java.time.LocalDateTime;
@@ -10,13 +13,24 @@ import java.time.LocalDateTime;
 public class Point {
 
     @Id
-    @GeneratedValue(strategy = jakarta.persistence.GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotNull
     private Long userId;
 
+    @NotNull
+    @PositiveOrZero
+    private Long amount;
+
     @Enumerated(value = EnumType.STRING)
+    @NotNull
     private PointType type;
 
+    @NotNull
     private LocalDateTime createdAt;
+
+    @NotNull
+    @PositiveOrZero
+    private Long remainPoint;
 }
