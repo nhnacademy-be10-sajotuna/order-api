b0ee61ac0026840362a4d3bd833fa408830b688f
jeonseung-u <junseungu74@naver.com>
2025-06-12 14:20:08 +0900

    [chore] : 사용하지 않은 import 제거

--- src/main/java/shop/sajotuna/order/orders/controller/OrderController.java
+++ src/main/java/shop/sajotuna/order/orders/controller/OrderController.java
@@ -1,7 +1,6 @@
 package shop.sajotuna.order.orders.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import shop.sajotuna.order.orders.dto.OrderRequest;
