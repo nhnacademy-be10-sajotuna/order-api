23956cfa9160baf259371031001eefada45e62df
jeonseung-u <junseungu74@naver.com>
2025-06-12 11:36:15 +0900

    [feat] : 주문 조회, 회원 주문, 비회원 주문, 주문 상품 조회, 수정
    - OrderService에 주문 조회, 회원의 주문 목록 조회 기능 구현
    - 비회원 주문, 회원 주문 일부분 구현 (결제, 쿠폰, 포인트도 처리되도록 구현 필요)
    - 주문 상품, 주문ID에 포함된 상품들 조회 구현
    - 주문 상품의 배송 상태 수정 구현

--- src/main/java/shop/sajotuna/order/orders/controller/OrderController.java
+++ src/main/java/shop/sajotuna/order/orders/controller/OrderController.java
@@ -1,11 +1,15 @@
 package shop.sajotuna.order.orders.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import shop.sajotuna.order.orders.dto.OrderRequest;
 import shop.sajotuna.order.orders.dto.GuestOrderRequest;
+import shop.sajotuna.order.orders.dto.OrderResponse;
 import shop.sajotuna.order.orders.service.OrderService;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/orders")
@@ -16,26 +20,27 @@ public class OrderController {
 
     // 주문 조회
     @GetMapping("/{orderId}")
-    public ResponseEntity<?> getOrder(@PathVariable Long orderId){
-        return null;
+    public ResponseEntity<OrderResponse> getOrder(@PathVariable Long orderId){
+        return new ResponseEntity<>(orderService.findOrder(orderId), HttpStatus.OK);
     }
 
     // 회원의 주문내역 조회
     @GetMapping("/user")
-    public ResponseEntity<?> getUserOrder(@RequestParam Long userId){
-        return null;
+    public ResponseEntity<List<OrderResponse>> getUserOrder(@RequestParam Long userId){
+        return new ResponseEntity<>(orderService.findOrdersByUserId(userId), HttpStatus.OK);
     }
 
     // 회원 주문
     @PostMapping("/user")
-    public ResponseEntity<?> createUserOrders(@RequestBody OrderRequest request) {
-        return null;
+    public ResponseEntity<OrderResponse> createUserOrders(@RequestBody OrderRequest request) {
+
+        return new ResponseEntity<>(orderService.createUserOrder(request), HttpStatus.OK);
     }
 
     // 비회원 주문
     @PostMapping("/guest")
-    public ResponseEntity<?> createGuestOrders(@RequestBody GuestOrderRequest request) {
+    public ResponseEntity<OrderResponse> createGuestOrders(@RequestBody GuestOrderRequest request) {
 
-        return null;
+        return new ResponseEntity<>(orderService.createGuestOrder(request), HttpStatus.OK);
     }
 }
