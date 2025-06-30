e525f722dc9722ab98405a062e9843578a903e0e
sanikani <sanikani@naver.com>
2025-06-30 16:37:38 +0900

    [feat] : 순수 주문 금액 계산 구현
    - 3개월 간 순수 주문 금액 계산
    - 순수 상품 금액 - (쿠폰 + 포인트)

--- src/main/java/shop/sajotuna/order/orders/repository/OrderRepository.java
+++ src/main/java/shop/sajotuna/order/orders/repository/OrderRepository.java
@@ -22,4 +22,6 @@ public interface OrderRepository extends JpaRepository<Order, Long> {
     List<Order> findShippedOrders(LocalDateTime time);
 
     Order findOrderByOrderNumber(String orderNumber);
+
+    List<Order> findByOrdererUserIdAndCreatedAtAfter(Long userId, LocalDateTime createdAt);
 }
---
+++ src/main/java/shop/sajotuna/order/orders/service/OrderTotalPriceService.java
@@ -0,0 +1,26 @@
+package shop.sajotuna.order.orders.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import shop.sajotuna.order.common.domain.Money;
+import shop.sajotuna.order.orders.domain.Order;
+import shop.sajotuna.order.orders.repository.OrderRepository;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class OrderTotalPriceService {
+
+    private final OrderRepository orderRepository;
+
+    // 3개월 간 순수 주문 금액 계산
+    public Money calculateTotalOrderAmount(Long userId) {
+        LocalDateTime threeMonthsAgo = LocalDateTime.now().minusMonths(3);
+        List<Order> orders = orderRepository.findByOrdererUserIdAndCreatedAtAfter(userId, threeMonthsAgo);
+        return orders.stream()
+                .map(Order::getFinalProductPrice)
+                .reduce(Money.zero(), Money::plus);
+    }
+}
